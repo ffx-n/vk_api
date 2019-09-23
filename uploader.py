f7996650b2f3e22fe6b93fa4d4022dda3c46dcdf
ffx-n <53563773+ffx-n@users.noreply.github.com>
2019-09-23 09:10:21 +0500

    Update uploader.py

--- uploader.py
+++ uploader.py
@@ -88,7 +88,7 @@ def sendd_comment(num,access_token):
     except:
         print('Разгадываю капчу')
     balance = check_balance(RUCAPTCHA_KEY)
-    if float(balance)>25:
+    if float(balance)>20:
         try:
             sid = text['error']['captcha_sid']
             image_link = text['error']['captcha_img']
